ad44a1cc6f20b48841fa0d1c9934e86a181a2c07
xVanTuring <xVanTuring@foxmail.com>
2025-12-09 20:44:21 +0800

    Initial Commit

---
+++ OSSBrowser/OSSBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSSBrowserApp.swift
+//  OSSBrowser
+//
+//  Created by xvan on 2025/12/9.
+//
+
+import SwiftUI
+
+@main
+struct OSSBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
